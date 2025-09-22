3210ea1a4c85c4a4dd958067d56e6a65f6b9b8ae
eden1711 <taitt171199@gmail.com>
2025-09-22 22:10:33 +0700

    code structure

---
+++ src/lib/components/layout/header/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import CartIcon from '$lib/components/icons/CartIcon.svelte';
+	import MenuIcon from '$lib/components/icons/MenuIcon.svelte';
+</script>
+
+<header class="header flex h-16 items-center justify-between bg-white">
+	<MenuIcon />
+	<h1 class="">Catchy Flower</h1>
+	<CartIcon />
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { Header } from '$lib/components/layout/header';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="container mx-auto min-h-screen px-4 py-2">
+	<Header />
+	{@render children?.()}
+</div>
